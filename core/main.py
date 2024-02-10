303c77cf796c460f9d392df9b7b38a86e1214363
Med. Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-02-10 10:29:44 +0000

    code: added sys argument

--- core/main.py
+++ core/main.py
@@ -1,8 +1,14 @@
+import sys
+
 import nltk
 from nltk.chat.util import Chat
 
 from dataset import data 
 
+input_v = sys.argv[1]
 
 chat = Chat(data)
-chat.converse()
+
+result = chat.respond(input_v)
+
+print(result)
