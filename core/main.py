0e4d8f8ab4b9a9e5de6e9c184a3d2b491461f555
Med. Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-02-10 00:13:11 +0000

    fix: no reflections needed

--- core/main.py
+++ core/main.py
@@ -1,10 +1,8 @@
 import nltk
-from nltk.chat.util import Chat, reflections
+from nltk.chat.util import Chat
 
-from reflections import custom_reflections
 from dataset import data 
 
-reflections.update(custom_reflections)
 
-chat = Chat(data, reflections)
+chat = Chat(data)
 chat.converse()
