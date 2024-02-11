a84a0a482614e778518cc18bf182a7aac5685fbe
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-02-11 16:14:51 +0100

    Revert "added cors for fixing interactivity issues with UI"

--- core/dataset.py
+++
@@ -1,7 +0,0 @@
-data = [
-    (r'(3aslema|selem|hi|hello|hellowers|cha7welek|slm|salut)(.*)', ['ahla bik, m3ak fsbGPT inspiré mel produits mte3 OPENAI ama hedheya demo, chnowa t7eb ta3ref 3la FSB?']),
-    (r'(?i)(.*)(fsb)(.*)', ['t7esek ta7ki 3la fsb, alors heya faculté étatique f jarzouna fiha quelques départements kima INFORMATIQUE, PHYSIQUE, BIOLOGIE ... chnowa t7eb ta3ref e5er, ena memoire mte3i zghira barcha es2elni 3la profetna mte3 IA mathalan wla 3la étudiant meli na3mlou f présentation hedhi?']),
-    (r'(?i)(.*)(chkoun |chkounou )(dhia)(.*)', ['7asb kelemk t7esek t7eb ta3ref chkoun dhia, alors dhia howa eli 5dem mini projet hedheya, étudiant f GLSI2 w motivant w maghroun y7eb ya3mel solutions yfidou mojtama3 w yet3alem hajet jdida']),
-    (r'.*', ['t7esek kayenek ma3titni chy, jareb es2elni 3la esemi étudiants GLSI2 wla esm profa wla département fouleniya etc...']),
-    (r'', [''])
-]
--- types/global.d.ts
+++
@@ -1,3 +0,0 @@
-interface chatbotUniqueMessageEx {
-    message: String;
-}
