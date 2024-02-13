d0f1bf0117c184e913e9ec15ebc4f7536d5842f6
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-02-13 22:49:36 +0100

    code: chatBox UI changed new theme

--- app-ui/src/components/chatBox.tsx
+++ app-ui/src/components/chatBox.tsx
@@ -1,25 +1,30 @@
 import { TypeAnimation } from "react-type-animation";
 
-export default function chatBox({ chat, setTypingStatus}) {
+export default function chatBox({ chat, setTypingStatus }: ChatBoxProps) {
   return (
-    <div className="flex-1 m-2 rounded-md p-2 bg-custom">
+    <div className="flex-1 m-2 overflow-auto rounded-md p-2 bg-custom space-y-2">
       {chat.map((e, index) => (
         <div
           key={index}
-          className={`max-w-4xl mx-auto border-[1px] py-3 my-1 px-2 rounded-md ${e.role === "fsb-GPT-demo" ? "bg-red-500 text-white" : "bg-gray-300 text-gray-800"
+          className={`max-w-4xl mx-auto border-[1px] py-3 my-1 px-2 rounded-md backdrop bg-white bg-opacity-10 text-white shadow-lg ${e.role === "fsb-GPT-demo" ? "border-[#1F71BA]" : "border-white"
             }`}
         >
-          <p className="font-extrabold">{e.role}</p>
           {index % 2 === 1 ? (
-            <p className="text-black">{e.message}</p>
+            <>
+              <p className="font-extrabold text-lg py-1">{e.role}</p>
+              <p className="text-white">{e.message}</p>
+            </>
           ) :
             (
-              <TypeAnimation
-                className={`text-black`}
-                sequence={[200, () => {setTypingStatus(true)}, 3000, e.message, ()=> {setTypingStatus(false)}]}
-                cursor={false}
-                repeat={0}
-              />
+              <>
+                <p className=" font-extrabold text-[#91c5f3] text-lg py-1"><span className=" shadow-lg shadow-gray-500 text-xl mr-1">🤖</span>{e.role}</p>
+                <TypeAnimation
+                  className={`text-white`}
+                  sequence={[200, () => { setTypingStatus(true) }, 3000, e.message, () => { setTypingStatus(false) }]}
+                  cursor={false}
+                  repeat={0}
+                />
+              </>
             )}
         </div>
       ))}
