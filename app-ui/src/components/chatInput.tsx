d439c6edc569d7d09c4688a1a0349594e4209add
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-02-13 23:17:29 +0100

    code: disabled theme  added when typing

--- app-ui/src/components/chatInput.tsx
+++ app-ui/src/components/chatInput.tsx
@@ -3,7 +3,7 @@ export default function chatInput({ q, setQ, handleKeyDown, fetchReply, typingSt
     <div className="w-full flex justify-center gap-x-2 my-3">
       <input
         placeholder="Aya 9oli haja"
-        className="placeholder:text-white placeholder:opacity-30 px-4 py-3 w-9/12 outline-none rounded-md border-2 border-[#1F71BA] bg-gray-700 text-white"
+        className="placeholder:text-white disabled:border-gray-600 disabled:cursor-no-drop placeholder:opacity-30 px-4 py-3 w-9/12 outline-none rounded-md border-2 border-[#1F71BA] bg-gray-700 text-white"
         type="text"
         value={q}
         onChange={(e) => setQ(e.target.value)}
@@ -13,7 +13,7 @@ export default function chatInput({ q, setQ, handleKeyDown, fetchReply, typingSt
       <button
         onClick={() => fetchReply()}
         onTouchStart={() => fetchReply()}
-        className="w-3/12 sm:w-2/12 bg-[#1F71BA] rounded-md px-2 hover:bg-[#1F71BA] transition duration-300 text-white font-semibold"
+        className="w-3/12 sm:w-2/12 bg-[#1F71BA] disabled:bg-gray-600 disabled:cursor-no-drop rounded-md px-2 hover:bg-[#1F71BA] transition duration-300 text-white font-semibold"
         disabled={typingStatus}
       >
         Ab3ath
